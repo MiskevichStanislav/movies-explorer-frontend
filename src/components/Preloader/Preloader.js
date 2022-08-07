76b4f5568abd4554001ec92fd38c40fc258a1267
Miskevich Stanislav <miskevich95@yandex.ru>
2022-08-08 02:13:53 +0300

    creating and implementing  Preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -3,9 +3,11 @@ import './Preloader.css';
 const Preloader = () => {
     return (
         <div className="preloader">
-
+            <div className="preloader__container">
+                <span className="preloader__circle"></span>
+            </div>
         </div>
-    );
-}
+    )
+};
 
 export default Preloader;
