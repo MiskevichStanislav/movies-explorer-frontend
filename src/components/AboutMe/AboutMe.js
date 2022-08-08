df25d83049d74c7eaab27a44fb9cfb384633287c
Miskevich Stanislav <miskevich95@yandex.ru>
2022-08-08 05:40:33 +0300

    +++

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -14,8 +14,8 @@ function AboutMe() {
                         <p className="about-me__text">Я родился в сибирском городке Томск, закончил бакалавриат строительного факультета,закончил магистратуру строительного факультета Томского Государственного Архитектурно-Строительного Университета. Я люблю путешествовать,люблю много снимать фото и видео красивых локаций, а ещё увлекаюсь бегом и занимаюсь футболом. Ровно 9 месяцев назад, решил дополнить свою жизнь совершенно новой для меня сферой деятельности и ни разу не разочаровался. После того, как начал проходить курс по веб-разработке, решил пробовать себя во фрилансе.
                         </p>
                         <ul className="about-me__links">
-                            <li><a className="about-me__link" href="https://vk.com/miskevichstasik">Вконтакте</a></li>
-                            <li><a className="about-me__link" href="https://github.com/MiskevichStanislav">Github</a></li>
+                            <li><a className="about-me__link" href="https://vk.com/miskevichstasik" target="_blank" rel="noreferrer">Вконтакте</a></li>
+                            <li><a className="about-me__link" href="https://github.com/MiskevichStanislav" target="_blank" rel="noreferrer">Github</a></li>
                         </ul>
                     </div>
                     <img className="about-me__photo" src={photo} alt="Фото для портфолио" />
